6800f4822d31a127a8662d4ef11c46e58e753540
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-15 01:43:13 -0500

    views: tmp edit track list view for skinning before adding MissionStatus
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/mission_board/puzzle_hero/views.py
+++ src/mission_board/puzzle_hero/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import TrackStatus
-
+from .models import Track
+from .models import Mission
 
 # Create your views here.
 def index(request):
-    return render(request, "base.html")
+	return render(request, "base.html")
 
 
 class MissionBoardHome(ListView):
-    model = TrackStatus
-    context_object_name = 'trackstatus'
-    template_name = 'puzzle_hero/mission_board_home.html'
+	model = Track
+	context_object_name = 'tracks'
+	template_name = 'puzzle_hero/mission_board_home.html'
 
-    def get_queryset(self):
-        return TrackStatus.objects.filter(
-            team=self.request.user.player.team
-        )
-
-
+	def get_queryset(self):
+		tracks = Track.objects.all()
+		for track in tracks:
+			track.missions = Mission.objects.filter(track=track)
+		return tracks
