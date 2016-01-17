d8dce674fd03eaf53f12eacdaa99aa2631125712
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2016-01-17 14:59:40 -0500

    Simpler feedback on auth

--- src/mission_board/cs_auth/views.py
+++ src/mission_board/cs_auth/views.py
@@ -1,46 +1,42 @@
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.views import logout
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
-from django.views.generic import RedirectView
 from django.views.generic.edit import FormView
+from django.views.generic import RedirectView
+
 
 from .forms import LoginForm, RegisterForm
 
 
-class LoginView(FormView):
+class LoginView(SuccessMessageMixin, FormView):
     template_name = "cs_auth/form_view.html"
     form_class = LoginForm
     success_url = "/"
-    success_message = ("Successfully logged in as "
-                       "%(first_name)s %(last_name)s."
-                       "(%(username)s).")
+    success_message = "Successfully logged in!"
 
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super(LoginView, self).form_valid(form)
 
-    def get_success_message(self, cleaned_data):
-        return self.success_message % dict(cleaned_data,
-                                           first_name=self.object.first_name,
-                                           last_name=self.object.last_name,
-                                           username=self.object.username)
-
 
 class LogoutView(RedirectView):
     url = settings.LOGIN_URL
     permanent = True
+    success_message = "Logged out!"
 
     def dispatch(self, request, *args, **kwargs):
         logout(self.request)
         return super(LogoutView, self).dispatch(request, *args, **kwargs)
 
 
-class RegisterView(FormView):
+class RegisterView(SuccessMessageMixin, FormView):
     template_name = "cs_auth/form_view.html"
     form_class = RegisterForm
     success_url = "/"
+    success_message = "Successfully registered!"
 
     def form_valid(self, form):
         form.save()
