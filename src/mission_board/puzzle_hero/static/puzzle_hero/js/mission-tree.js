a634827d6e6437c501462a64286229652ae12275
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-23 14:16:36 -0500

    js: mission tree script expect for json data encoded as base64
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/mission_board/puzzle_hero/static/puzzle_hero/js/mission-tree.js
+++ src/mission_board/puzzle_hero/static/puzzle_hero/js/mission-tree.js
@@ -36,66 +36,70 @@
     }
   };
 
-  // Set up zoom support
-  var svg = d3.select("svg"),
-      inner = svg.select("g"),
-      zoom = d3.behavior.zoom().on("zoom", function() {
-        inner.attr("transform", "translate(" + d3.event.translate + ")" +
-                                    "scale(" + d3.event.scale + ")");
-      });
-  svg.call(zoom);
+function drawTree(selector, missions) {
+	var svg = d3.select(selector)
+    var inner = svg.select("g")
+	var render = new dagreD3.render();
 
-  var render = new dagreD3.render();
+	// Left-to-right layout
+	var g = new dagreD3.graphlib.Graph();
+	g.setGraph({
+		nodesep: 20,
+		ranksep: 40,
+		rankdir: "LR",
+		marginx: 10,
+		marginy: 10
+	});
 
-  // Left-to-right layout
-  var g = new dagreD3.graphlib.Graph();
-  g.setGraph({
-    nodesep: 20,
-    ranksep: 40,
-    rankdir: "LR",
-    marginx: 10,
-    marginy: 10
-  });
+	function draw(isUpdate) {
+		for (var id in missions) {
+			var mission = missions[id];
+			var className = mission.status;
+			var html = "<div>";
+			html += "<span class=reward>"+mission.reward+"</span>";
+			// html += '<button type="button" class="btn btn-danger" data-toggle="popover" title="Popover title" data-content="And heres some amazing content. Its very engaging. Right?">r</button>' #}
+			html += "</div>";
+			g.setNode(id, {
+				labelType: "html",
+				label: html,
+				rx: 5,
+				ry: 5,
+				padding: 0,
+				class: className
+			});
 
-  function draw(isUpdate) {
-    for (var id in missions) {
-      var mission = missions[id];
-      var className = mission.status;
-      var html = "<div>";
-      html += "<span class=reward>"+mission.reward+"</span>";
-	  // html += '<button type="button" class="btn btn-danger" data-toggle="popover" title="Popover title" data-content="And heres some amazing content. Its very engaging. Right?">r</button>' #}
-      html += "</div>";
-      g.setNode(id, {
-        labelType: "html",
-        label: html,
-        rx: 5,
-        ry: 5,
-        padding: 0,
-        class: className
-      });
+			for(i in mission.dependencies) {
+				var did = mission.dependencies[i];
+				g.setEdge(did, id, {
+					width: 40,
+					class: missions[did].status
+				});
+			}
+		}
 
-	  for(i in mission.dependencies) {
-		  var did = mission.dependencies[i];
-        g.setEdge(did, id, {
-          width: 40,
-		  class: missions[did].status
-        });
-      }
-    }
+		inner.call(render, g);
 
-    inner.call(render, g);
+		// Zoom and scale to fit
+		var graphWidth = g.graph().width;
+		var graphHeight = g.graph().height;
+		var width = parseInt(svg.style("width").replace(/px/, ""));
+		var height = parseInt(svg.style("height").replace(/px/, ""));
+		var zoomScale = Math.min(width / graphWidth, height / graphHeight);
+		var translate = [(width/2) - ((graphWidth*zoomScale)/2), (height/2) - ((graphHeight*zoomScale)/2)];
 
-    // Zoom and scale to fit
-    var graphWidth = g.graph().width;
-    var graphHeight = g.graph().height;
-    var width = parseInt(svg.style("width").replace(/px/, ""));
-    var height = parseInt(svg.style("height").replace(/px/, ""));
-    var zoomScale = Math.min(width / graphWidth, height / graphHeight);
-    var translate = [(width/2) - ((graphWidth*zoomScale)/2), (height/2) - ((graphHeight*zoomScale)/2)];
-    zoom.translate(translate);
-    zoom.scale(zoomScale);
-    zoom.event(isUpdate ? svg.transition().duration(500) : d3.select("svg"));
-  }
+		var zoom = d3.behavior.zoom().on("zoom", function() {
+			inner.attr("transform", "translate(" + d3.event.translate + ")" +
+				"scale(" + d3.event.scale + ")");
+		});
+		zoom.translate(translate);
+		zoom.scale(zoomScale);
+		zoom.event(isUpdate ? svg.transition().duration(500) : d3.select("svg"));
+	}
 
-  draw();
+	draw();
+}
 
+$(".track-tree").each(function(index, item) {
+	var json = atob($(item).data('json'))
+	drawTree(item, missions);
+});
