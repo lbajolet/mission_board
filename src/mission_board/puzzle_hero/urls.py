1362385e68fb6b5769bb87401db2d0ffbe479462
Alexandre Terrasa <alexandre@moz-code.org>
2016-01-23 14:15:47 -0500

    scoreboard: change team url to team/
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/mission_board/puzzle_hero/urls.py
+++ src/mission_board/puzzle_hero/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from .views import submit_flag, team_stats, Scoreboard, TracksList, MissionPage
 
 urlpatterns = [
-    url(r'^team_stats$', team_stats, name="team_stats"),
     url(r'^flag/$', submit_flag, name="submit_flag"),
+    url(r'^team$', team_stats, name="team_stats"),
     url(r'^missions/(?P<mission>\w+)$', MissionPage.as_view(), name="mission_page"),
     url(r'^missions$', TracksList.as_view(), name="tracklist"),
     url(r'^$', Scoreboard.as_view(), name="scoreboard"),
