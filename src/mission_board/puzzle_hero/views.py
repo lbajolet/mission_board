d41742a72ef73a83b1e9a0a4aa03af65b5892791
Alexandre Terrasa <alexandre@moz-code.org>
2016-02-27 01:04:45 -0500

    views: fix icon display in track_details view
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- src/mission_board/puzzle_hero/views.py
+++ src/mission_board/puzzle_hero/views.py
@@ -223,6 +223,7 @@ class TrackDetail(LoginRequiredMixin, UserPassesTestMixin, DetailView):
             mission_data["title"] = ms.mission.title
             mission_data["status"] = ms.status
             mission_data["reward"] = ms.mission.reward
+            mission_data["kind"] = ms.mission.kind
 
             dep_data = []
             for dep in ms.mission.dependencies.all():
